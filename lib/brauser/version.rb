29d8dd3a2c7cf913a3f3036697323bcb64f48621
ShogunPanda <shogun_panda@me.com>
2013-07-14 15:25:25 -0700

    Version 3.1.4

--- lib/brauser/version.rb
+++ lib/brauser/version.rb
@@ -16,7 +16,7 @@ module Brauser
     MINOR = 1
 
     # The patch version.
-    PATCH = 3
+    PATCH = 4
 
     # The current version of brauser.
     STRING = [MAJOR, MINOR, PATCH].compact.join(".")
